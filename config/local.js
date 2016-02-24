aea84427d90d0bc7868612b1702ce674d2ee378a
cristian <cfpena@espol.edu.ec>
2016-02-24 01:36:44 -0500

    arreglos de front end

--- config/local.js
+++ config/local.js
@@ -60,7 +60,8 @@ module.exports = {
    * to 80 (http://) or 443 (https://) if you have an SSL certificate        *
    ***************************************************************************/
 
-  // port: process.env.PORT || 1337,
+  port: 80,
+  appName: "dawChart",
 
   /***************************************************************************
    * The runtime "environment" of your Sails app is either typically         *
